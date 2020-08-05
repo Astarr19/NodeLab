0625db4f6664a0873346e525e458dd44b0fbaeea
Aidan Starr <aidan.starr19@gmail.com>
2020-08-05 19:00:02 -0400

    correct code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let http = require('http');
 let quotes = require('./quotes');
 let server = (req, res) => {
     res.writeHead(200, {"Content-type": "Text/plain"});
-    let random = Math.floor(Math.random() * facts.length);
+    let random = Math.floor(Math.random() * quotes.length);
     res.write(quotes[random]);
     res.end();
 }
